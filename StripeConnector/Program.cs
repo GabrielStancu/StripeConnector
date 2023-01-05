e7a603f8ba958b58597758f2004cc20b8bda8452
GabrielStancu <gabriel.stancu07@yahoo.com>
2023-01-05 09:00:48 +0200

    CORS settings

--- StripeConnector/Program.cs
+++ StripeConnector/Program.cs
@@ -6,6 +6,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddCors(opt => {
+    opt.AddPolicy(name: "AllowOrigins", builder => {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 builder.Services.AddServices();
 builder.Services.AddStripe(builder.Configuration);
 
@@ -14,6 +21,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.UseCors("AllowOrigins");
 app.UseAuthorization();
 app.MapControllers();
 
